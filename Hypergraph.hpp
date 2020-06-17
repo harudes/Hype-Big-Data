270c022ee6f8ac39efcc4353d98422796d57f359
harudes <luis.rendon@ucsp.edu.pe>
2020-06-17 17:53:42 -0500

    definidos métodos de hypergrafo

--- Hypergraph.hpp
+++ Hypergraph.hpp
@@ -4,6 +4,23 @@
 class HyperGraph{
     std::map<int,std::set<int> > edges;
     std::map<int,std::set<int> > vertex;
+    std::map<int,double> neighbourMap;
 public:
     HyperGraph() = default;
+    void addVertex();
+    void addEdge();
+    void addEdgeList();
+    void addNodeList();
+    void getEdgesizeOfPercentBiggestEdge();
+    void connect(int v, int e);
+    auto getEdges();
+    auto getVertices();
+    void deleteVertex();
+    auto getEdgesOf();
+    auto getVerticesOf();
+    auto getSSetCandidates();
+    auto getNodeHeuristicExactly();
+    auto getNodeHeuristicEstimate();
+    auto getRandomNode();
+    auto getANode();
 };
